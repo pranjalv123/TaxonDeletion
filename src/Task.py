506c1b5de44070f76d6884057495f8212a89f011
Pranjal Vachaspati <pranjal@pranj.al>
2015-12-29 14:22:53 -0700

    minor change

--- src/Task.py
+++ src/Task.py
@@ -165,7 +165,7 @@ class Task(object):
                     return self.result
         except Exception as e:
             print "Couldn't read file!", filename
-            print e
+            raise e
         if not self.status() == "ready":
             raise DependenciesNotCompleteException
         self.result = self.run()
